2644e6f0dc90373725b8032e8729b077bbbc7e14
HaDucDev <haductrung392000@gmail.com>
2022-11-17 00:31:36 +0700

    configuration

---
+++ Hibernate_be/src/main/java/HibernateUtils.java
@@ -0,0 +1,39 @@
+import org.hibernate.SessionFactory;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+
+public class HibernateUtils
+{// đê nó hàng số cấp một lần thôi - static để có thể dùng chung luôn
+    private static final SessionFactory FACTORY ;
+
+    static {
+        Configuration conf=new Configuration();
+        Properties props=new Properties();
+        conf.setProperties(props);
+        props.put(Environment.DIALECT,">org.hibernate.dialect.MySQL8Dialect");
+        props.put(Environment.DRIVER,"com.mysql.cj.jdbc.Drive");
+        props.put(Environment.URL,"jdbc:mysql://localhost:3306/demobe");
+        props.put(Environment.USER,"root");
+        props.put(Environment.PASS,"123456");
+        props.put(Environment.SHOW_SQL,"true");// bật cái này để khi truy vấn bằng code java hibernate tựu sinh code sql cho ta xem ở cmd
+
+
+        ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+
+        FACTORY=conf.buildSessionFactory(registry);
+
+    }
+
+    // này này trong intelij ko tự sinh như eclipse đc. mình phai tự code
+    public static  SessionFactory getFactory(){
+        return FACTORY;
+    }
+   //ko thể thay đổi nên ko có hàm set
+
+}
