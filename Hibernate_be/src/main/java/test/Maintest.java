e01ec61a0f764fb272d86ee126cf394ad2d5ce11
HaDucDev <haductrung392000@gmail.com>
2022-11-17 17:42:58 +0700

    add-product-lk-n-1

--- Hibernate_be/src/main/java/test/Maintest.java
+++ Hibernate_be/src/main/java/test/Maintest.java
@@ -3,9 +3,11 @@ package test;
 
 import org.hibernate.Session;
 import pojo.Category;
+import pojo.Product;
 import utils.HibernateUtils;
 
 import javax.persistence.Query;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class Maintest {
@@ -29,7 +31,14 @@ public class Maintest {
 //
 //        list.forEach( c -> System.out.printf("%d - %s", c.getId(), c.getName()));
 
-        
+        Product p = new Product();
+        p.setName("Xe máy trắng");
+        p.setPrice(new BigDecimal(1589000000));
+
+        Category c = session.get(Category.class,2);
+        p.setCategory(c);
+
+        session.save(p);
 
 
         session.close();
