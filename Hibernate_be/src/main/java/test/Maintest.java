eb59ca681fd7a0e009cc63540a4298fca61e61ce
HaDucDev <haductrung392000@gmail.com>
2022-11-17 15:53:54 +0700

    HQL - List

--- Hibernate_be/src/main/java/test/Maintest.java
+++ Hibernate_be/src/main/java/test/Maintest.java
@@ -5,6 +5,9 @@ import org.hibernate.Session;
 import pojo.Category;
 import utils.HibernateUtils;
 
+import javax.persistence.Query;
+import java.util.List;
+
 public class Maintest {
     public static void main(String[] args) {
 
@@ -14,12 +17,18 @@ public class Maintest {
 //        a.setName("ô tô");
 //        a.setDescription("giá đắt quá");
 //        session.save(a);
-        Category b = session.get(Category.class,2); // trạng thái persistent
-        //b.setDescription("giá rẻ bèo vậy");
-        session.getTransaction().begin();
-        //session.save(b);
-        session.delete(b);
-        session.getTransaction().commit();
+//        Category b = session.get(Category.class,2); // trạng thái persistent
+//        //b.setDescription("giá rẻ bèo vậy");
+//        session.getTransaction().begin();
+//        //session.save(b);
+//        session.delete(b);
+//        session.getTransaction().commit();
+
+       Query q = session.createQuery("FROM Category");// HQL
+        List<Category> list= q.getResultList();
+
+        list.forEach( c -> System.out.printf("%d - %s", c.getId(), c.getName()));
+
 
         session.close();
     }
