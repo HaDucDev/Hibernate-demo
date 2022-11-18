a37400360738aa99d9e04c057cebba5771d60f84
HaDucDev <haductrung392000@gmail.com>
2022-11-18 14:12:34 +0700

    criteria-api-get-like-english

--- Hibernate_be/src/main/java/test/Maintest.java
+++ Hibernate_be/src/main/java/test/Maintest.java
@@ -10,6 +10,7 @@ import utils.HibernateUtils;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.LinkedHashSet;
@@ -52,29 +53,30 @@ public class Maintest {
 //        Category cate= session.get(Category.class,2);
 //        cate.getProductList().forEach(p -> System.out.printf("%d - %s",p.getId(),p.getName()) );
 
-//        // them san pham manytomany
+        // them san pham manytomany
 //        Product p= new Product();
-//        p.setName("xe máy nâu trắng");
+//        p.setName("xe đạp vàng xanh");
 //        p.setDescription("rất rẻ");
-//        p.setPrice(new BigDecimal(123456789));
+//        p.setPrice(new BigDecimal(1898651224));
 //
 //
-//        Category c= session.get(Category.class,2);
+//        Category c= session.get(Category.class,3);
 //        p.setCategory(c);
 //
 //        Set<Manufacturer> manufacturerSet= new LinkedHashSet<>();
 //        // lay nha sx 1
 //        Manufacturer n1=session.get(Manufacturer.class,1);
 //        manufacturerSet.add(n1);
-//        // lay nha sx 2
+//         //lay nha sx 2
 //        Manufacturer n2=session.get(Manufacturer.class,2);
 //        manufacturerSet.add(n2);
 //
-//        // có viêt tat
-//       // manufacturerSet.add(session.get(Manufacturer.class,1));
-//        //manufacturerSet.add(session.get(Manufacturer.class,2));
-//        p.setManufacturerSet(manufacturerSet);
+////        // có viêt tat
+////       // manufacturerSet.add(session.get(Manufacturer.class,1));
+////        //manufacturerSet.add(session.get(Manufacturer.class,2));
 //
+//
+//        p.setManufacturerSet(manufacturerSet);
 //        session.getTransaction().begin();
 //        session.save(p);
 //        session.getTransaction().commit();;
@@ -85,6 +87,20 @@ public class Maintest {
 
 
         // criteria query api
+//        CriteriaBuilder builder= session.getCriteriaBuilder();
+//        CriteriaQuery<Product> query= builder.createQuery(Product.class);
+//        // mấy mấy bảng thì tạo mấy root. muốn joinn 2 bảng thì tạo 2 root
+//        Root root = query.from(Product.class);
+//
+//        query =query.select(root); // lấy hết bảng
+//
+//
+//        Query q=session.createQuery(query);// thành ra 4 dòng trên chỉ để xây dựng đối tượng truy vấn thôi.
+//        List<Product> productList=q.getResultList();  //getSingleResult() lấy phần tử đầu tiên
+//        productList.forEach( a -> System.out.printf("%d - %s- %s",a.getId(),a.getName(),a.getPrice()));
+
+
+         //criteria query api 123
         CriteriaBuilder builder= session.getCriteriaBuilder();
         CriteriaQuery<Product> query= builder.createQuery(Product.class);
         // mấy mấy bảng thì tạo mấy root. muốn joinn 2 bảng thì tạo 2 root
@@ -93,9 +109,17 @@ public class Maintest {
         query =query.select(root); // lấy hết bảng
 
 
-        Query q=session.createQuery(query);// thành ra 4 dòng trên chỉ để xây dựng đối tượng truy vấn thôi.
-        List<Product> productList=q.getResultList();  //getSingleResult() lấy phần tử đầu tiên
-        productList.forEach( a -> System.out.printf("%d - %s- %s",a.getId(),a.getName(),a.getPrice()));
+        Predicate p1=builder.like(root.get("name").as(String.class),"%xe_may%" );
+                // lấy name trong đối tượng product là kiểu chuỗi
+        Predicate p2=builder.like(root.get("name").as(String.class),"%xe_dap%");
+
+        query= query.where(builder.or(p1,p2));
+
+        Query q=session.createQuery(query);
+        List<Product> productList= q.getResultList();
+        productList.forEach( a -> System.out.printf("%d - %s- %s\n",a.getId(),a.getName(),a.getPrice()));
+
+
 
 
         session.close();
