4e503af203dbd3f48c5e72ccb93174d4529bcc1b
HaDucDev <haductrung392000@gmail.com>
2022-11-18 09:48:59 +0700

    add-sp-manytoone

--- Hibernate_be/src/main/java/test/Maintest.java
+++ Hibernate_be/src/main/java/test/Maintest.java
@@ -3,12 +3,15 @@ package test;
 
 import org.hibernate.Session;
 import pojo.Category;
+import pojo.Manufacturer;
 import pojo.Product;
 import utils.HibernateUtils;
 
 import javax.persistence.Query;
 import java.math.BigDecimal;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Maintest {
     public static void main(String[] args) {
@@ -43,8 +46,36 @@ public class Maintest {
 //        Product p1= session.get(Product.class,4);
 //        System.out.println(p1.getName());
 
-        Category cate= session.get(Category.class,2);
-        cate.getProductList().forEach(p -> System.out.printf("%d - %s",p.getId(),p.getName()) );
+//        Category cate= session.get(Category.class,2);
+//        cate.getProductList().forEach(p -> System.out.printf("%d - %s",p.getId(),p.getName()) );
+
+        // them san pham manytomany
+        Product p= new Product();
+        p.setName("xe máy nâu trắng");
+        p.setDescription("rất rẻ");
+        p.setPrice(new BigDecimal(123456789));
+
+
+        Category c= session.get(Category.class,2);
+        p.setCategory(c);
+
+        Set<Manufacturer> manufacturerSet= new LinkedHashSet<>();
+        // lay nha sx 1
+        Manufacturer n1=session.get(Manufacturer.class,1);
+        manufacturerSet.add(n1);
+        // lay nha sx 2
+        Manufacturer n2=session.get(Manufacturer.class,2);
+        manufacturerSet.add(n2);
+
+        // có viêt tat
+       // manufacturerSet.add(session.get(Manufacturer.class,1));
+        //manufacturerSet.add(session.get(Manufacturer.class,2));
+        p.setManufacturerSet(manufacturerSet);
+
+        session.getTransaction().begin();
+        session.save(p);
+        session.getTransaction().commit();;
+
 
         session.close();
     }
